83f6120e24dff8e6ee2c406370dffa5791e1cc66
Romanchukmaxim <romanchukmaxim@gmail.com>
2020-04-04 00:09:38 +0300

    modified 2-3

--- 2-3.py
+++ 2-3.py
@@ -1,4 +1,4 @@
 a = input("input the first - ")
 b = input("input the second - ")
 a, b = b, a
-print ("Now the first is", a, '; and the second is', b)
+print (f"Now the first is {a}; and the second is {b}")
