2f8d35f3b8005b948723c11a6e273eb47a37cdf8
Romanchukmaxim <romanchukmaxim@gmail.com>
2020-04-20 16:23:35 +0300

    add 7-1-2

---
+++ 7-2.py
@@ -0,0 +1,5 @@
+s = "string"
+twos = ''
+for i in s:
+    twos = twos + i + i
+print(twos)
---
+++ 7-3.py
@@ -0,0 +1,5 @@
+def double_char(s):
+    sd = ''
+    for i in s:
+        sd = sd + i + i
+    return(sd)
